86602c560ed60201472e76aa11d198df74361543
Michael <michael.alexander0008@gmail.com>
2024-06-18 14:17:11 +0100

    Complete Exercise 6.12

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteFilter from './components/AnecdoteFilter'
+import Notification from './components/Notification'
 
 const App = () => {
 
   return (
     <div>
+      <Notification />
       <h2>Anecdotes</h2>
       <AnecdoteFilter />
       <AnecdoteList />
